075ce20be9be82a4722d5084c0d371c0f4a61bf3
smoot <smoothtacomann@gmail.com>
2019-06-18 15:41:02 -0400

    forloop to check for duplicate keypresses, needs debugging, pushing for progress

--- hangman.js
+++ hangman.js
@@ -5,6 +5,12 @@ let wordBank = [
   "The Goonies",
   "Halloween"
 ];
+
+let keysPressed = [];
+//sort keys for speed
+let sortedKeys = keysPressed.slice().sort();
+//results of forloop
+
 //find key pressed
 //display key pressed
 document.addEventListener("keydown", findLetter);
@@ -12,20 +18,28 @@ document.addEventListener("keydown", findLetter);
 function findLetter(event) {
   let alphabet = event.keyCode;
   //if letter matches keyCodes for alphabet show on screen else dont
-  if (
-    (alphabet > 64 && alphabet < 91) ||
-    (alphabet > 96 && alphabet < 123) ||
-    alphabet == 8
-  ) {
-    letter.textContent += ` ${event.key}`;
+  if ((alphabet > 64 && alphabet < 91) || (alphabet > 96 && alphabet < 123)) { 
+    for (let i = 0; sortedKeys.length - 1; i++) {
+      if (sortedKeys[i + 1] == sortedKeys[i]) {
+        // letter.textContent += ` ${event.key}`;
+        keysPressed.push(alphabet);
+        console.log(keysPressed);
+      
+      }
+    }
   } else {
-    return false;
+    return alert("key has already been pressed");
+
   }
 }
 
-//stop key from being pressed twice
+function chooseMovie(wordBank) {}
+
 //check if key matches letters of a word
 //if it matches show on spaces
 //only have spaces for how many letters in word
 //if person gets word right make a sound
 //go to next word
+//show letters you have already guessed
+
+//stop key from being pressed twice
