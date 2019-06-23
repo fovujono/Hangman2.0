6dc4a377083cf4a930e5cac8547e2862bcbd42ef
smoot <smoothtacomann@gmail.com>
2019-06-23 15:23:17 -0400

    pushing duplicates into seperate arr

--- hangman.js
+++ hangman.js
@@ -7,33 +7,34 @@ let wordBank = [
 ];
 
 let keysPressed = [];
-//sort keys for speed
-let sortedKeys = keysPressed.slice().sort();
-//results of forloop
+let duplicatesArray = [];
 
-//find key pressed
-//display key pressed
 document.addEventListener("keydown", findLetter);
 
 function findLetter(event) {
   let alphabet = event.keyCode;
-  //if letter matches keyCodes for alphabet show on screen else dont
-  if ((alphabet > 64 && alphabet < 91) || (alphabet > 96 && alphabet < 123)) { 
-    for (let i = 0; sortedKeys.length - 1; i++) {
-      if (sortedKeys[i + 1] == sortedKeys[i]) {
-        // letter.textContent += ` ${event.key}`;
-        keysPressed.push(alphabet);
-        console.log(keysPressed);
-      
-      }
-    }
-  } else {
-    return alert("key has already been pressed");
 
+  keysPressed.push(alphabet);
+
+  //sort keys for speed
+  let sortedKeys = keysPressed.slice().sort();
+  console.log("keys pressed" + keysPressed);
+  console.log("sorted" + sortedKeys);
+  // if letter matches keyCodes for alphabet show on screen else dont
+  if ((alphabet > 64 && alphabet < 91) || (alphabet > 96 && alphabet < 123)) {
+    for (let i = 0; i < sortedKeys.length - 1; i++) {
+      if (sortedKeys[i + 1] == sortedKeys[i]) {
+        duplicatesArray.push(sortedKeys[i]);
+      } 
+      // else if (duplicatesArray[i] > 0 && duplicatesArray[i] !== null) {
+      //   console.log("duplicatesArray" + duplicatesArray);
+      //   letter.textContent += ` ${event.key}`;
+      // }
+    }
+    console.log("Res"+ duplicatesArray)
   }
 }
-
-function chooseMovie(wordBank) {}
+// function chooseMovie(wordBank) {}
 
 //check if key matches letters of a word
 //if it matches show on spaces
